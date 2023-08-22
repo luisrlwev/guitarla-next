ad7870f943fe4c1d1743b400330ecc31c45d7992
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-22 16:44:55 -0500

    Creamos la ruta de nostros y agregamos el modelo basado en rutas

---
+++ pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Nosotros() {
+  return (
+    <div>
+        Desde nosotros.js
+        <Link href="/">Ir a inicio</Link>
+    </div>
+  )
+}
