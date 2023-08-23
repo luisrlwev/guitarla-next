9ebe0e6510002729d38587cecd0178999527570f
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-23 18:01:57 -0500

    Creamos el footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,30 @@
+import Link from "next/link"
+
+const styles = {}
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="contenedor">
+        <nav className={styles.navegacion}>
+          <Link href="/">
+              Inicio
+          </Link>
+
+          <Link href="/nosotros">
+              Nosotros
+          </Link>
+
+          <Link href="/tienda">
+              Tienda
+          </Link>
+
+          <Link href="/blog">
+              Blog
+          </Link>
+        </nav>
+        <p>Todos los derechos reservados {new Date().getFullYear()}.</p>
+      </div>
+    </footer>
   )
 }
