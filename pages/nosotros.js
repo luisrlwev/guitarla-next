bf5f1ca3e629a81ba77c79f51223a7e24566ed6e
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-23 15:43:59 -0500

    Creamos el layout principal

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,11 @@
+import Layout from "@/components/layout"
 import Link from "next/link"
 
 export default function Nosotros() {
   return (
-    <div>
+    <Layout>
         Desde nosotros.js
         <Link href="/">Ir a inicio</Link>
-    </div>
+    </Layout>
   )
 }
