23ca094a278a8476f9234c711c6580bedb8e3ed8
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-30 16:59:25 -0500

    Agregamos CSS a la tienda

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,16 +1,17 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from "@/styles/guitarras.module.css"
 
 export default function Guitarra({guitarra}) {
   const { descripcion, imagen, nombre, precio, url } = guitarra
   return (
-    <div>
+    <div className={styles.guitarra}>
       <Image src={imagen.data.attributes.formats.medium.url} width={600} height={400} alt={`Guitarra ${nombre}`}/>
-      <div>
+      <div className={styles.contenido}>
         <h3>{nombre}</h3>
-        <p>{descripcion}</p>
-        <p>${precio}</p>
-        <Link className="" href={`/guitarras/${url}`}>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
+        <Link className={styles.enlace} href={`/guitarras/${url}`}>
           Ver producto
         </Link>
       </div>
