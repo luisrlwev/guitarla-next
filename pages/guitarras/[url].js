ca146a30140c145246fd3862ae8079465dd4cc1c
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-30 17:40:37 -0500

    Hicimos routing dinamico con getServerSideProps

---
+++ pages/guitarras/[url].js
@@ -0,0 +1,16 @@
+export default function Producto({guitarra}) {
+    console.log(guitarra[0].attributes.nombre)
+  return (
+    <div>Producto</div>
+  )
+}
+
+export async function getServerSideProps({query: {url}}){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras/?filters[url]=${url}&populate=imagen`)
+    const {data: guitarra} = await respuesta.json()
+    return{
+        props: {
+            guitarra
+        }
+    }
+}
