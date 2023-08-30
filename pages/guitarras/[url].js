1eecde86ebf12b4359a9fa39d3eb43f1cc70f90a
Luis Angel Hernandez <rlwev@hotmail.com>
2023-08-30 17:58:18 -0500

    Consultamos el routing dinamico con getStaticPaths y getStaticProps

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -5,6 +5,33 @@ export default function Producto({guitarra}) {
   )
 }
 
+export async function getStaticPaths(){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras/`)
+    const { data } = await respuesta.json()
+
+    const paths = data.map(guitarra => ({
+        params: {
+            url: guitarra.attributes.url
+        }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params: {url}}){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras/?filters[url]=${url}&populate=imagen`)
+    const {data: guitarra} = await respuesta.json()
+    return{
+        props: {
+            guitarra
+        }
+    }
+}
+
+/*
 export async function getServerSideProps({query: {url}}){
     const respuesta = await fetch(`${process.env.API_URL}/guitarras/?filters[url]=${url}&populate=imagen`)
     const {data: guitarra} = await respuesta.json()
@@ -14,3 +41,4 @@ export async function getServerSideProps({query: {url}}){
         }
     }
 }
+*/
