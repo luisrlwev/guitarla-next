0a678cc50465f90ce4832e92dcd030d7f55faa95
Luis Angel Hernandez <rlwev@hotmail.com>
2023-09-04 17:02:37 -0500

    Calculamos el total del carrito de compras

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,8 +1,17 @@
-import Layout from "@/components/layout"
+import { useEffect, useState } from "react"
 import Image from "next/image"
+import Layout from "@/components/layout"
 import styles from '@/styles/carrito.module.css'
 
 export default function Carrito({carrito, actualizarCantidad}) {
+
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        const calculoTotal = carrito.reduce( (total, producto) => total + (producto.cantidad * producto.precio), 0 )
+        setTotal(calculoTotal)
+    }, [carrito])
+
   return (
     <Layout
         title="Carrito"
@@ -47,7 +56,7 @@ export default function Carrito({carrito, actualizarCantidad}) {
                 </div>
                 <aside className={styles.resumen}>
                     <h3>Resumen del pedido</h3>
-                    <p>Total a pagar: </p>
+                    <p>Total a pagar: $ {total}</p>
                 </aside>
             </div>
         </main>
